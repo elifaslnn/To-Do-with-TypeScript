b13756ef46c9c4953d5f359b461aed18456cab5f
Elif Aslan <elifaslann070@gmail.com>
2025-12-29 21:24:43 +0300

    import tailwind and created basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="text-xl text-center mt-10">TO-DO APP</h1>
+      <div className="grid items-center justify-items-center m-10">
+        <div className="w-5/6">
+          <input
+            placeholder="görev girin"
+            className="m-1 bg-amber-50 text-amber-800 w-6/8 rounded-md p-1"
+          ></input>
+          <button className="bg-blue-700 p-1 rounded-md w-1/8 ml-1">
+            ekle
+          </button>
+        </div>
+
+        <ul className="w-5/6 m-3">
+          <li className=" p-1 m-1 bg-gray-500 rounded-md w-full text-black">
+            görev1
+          </li>
+          <li className=" p-1 m-1 bg-gray-500 rounded-md w-full text-black">
+            görev2
+          </li>
+          <li className=" p-1 m-1 bg-gray-500 rounded-md w-full text-black">
+            görev3
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
