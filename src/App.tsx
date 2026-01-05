d7009e85e9addb86be503fd9f27028866b9268a8
Elif Aslan <elifaslann070@gmail.com>
2026-01-05 16:16:45 +0300

    create todo, delete todo and edit todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todoText, setTodoText] = useState<string>("");
+  interface Todo {
+    id: number;
+    text: string;
+    edit: boolean;
+  }
+  const [list, setList] = useState<Todo[]>([]);
+  const [editId, setEditId] = useState<number | null>();
+  const Id = useRef<number>(0);
+  const [editText, setEditText] = useState<string>("");
+
+  function addList(): void {
+    setList((prev) => [
+      ...prev,
+      { id: Id.current, text: todoText, edit: false },
+    ]);
+    Id.current++;
+  }
+
+  function deleteList(id: number): void {
+    setList((prev) => prev.filter((l) => l.id !== id));
+  }
+
+  function saveEditList() {
+    if (editId === null) return;
+
+    setList((prev) =>
+      prev.map((list) =>
+        list.id === editId ? { ...list, text: editText } : list
+      )
+    );
+
+    setEditId(null); // 👈 edit modundan çık
+    setEditText("");
+  }
+
   return (
     <>
       <h1 className="text-xl text-center mt-10">TO-DO APP</h1>
@@ -9,23 +45,62 @@ function App() {
         <div className="w-5/6">
           <input
             placeholder="görev girin"
-            className="m-1 bg-amber-50 text-amber-800 w-6/8 rounded-md p-1"
+            className="m-1 bg-amber-50 text-gray-900 w-6/8 rounded-md p-1"
+            value={todoText}
+            onChange={(e) => {
+              setTodoText(e.target.value);
+            }}
           ></input>
-          <button className="bg-blue-700 p-1 rounded-md w-1/8 ml-1">
+          <button
+            className="bg-blue-700 p-1 rounded-md w-1/8 ml-1"
+            onClick={() => addList()}
+          >
             ekle
           </button>
         </div>
 
         <ul className="w-5/6 m-3">
-          <li className=" p-1 m-1 bg-gray-500 rounded-md w-full text-black">
-            görev1
-          </li>
-          <li className=" p-1 m-1 bg-gray-500 rounded-md w-full text-black">
-            görev2
-          </li>
-          <li className=" p-1 m-1 bg-gray-500 rounded-md w-full text-black">
-            görev3
-          </li>
+          {list.map((l, index) => (
+            <li
+              key={index}
+              className="flex items-center justify-between p-1 m-1 bg-gray-500 rounded-md w-full text-black "
+            >
+              {editId === l.id ? (
+                <>
+                  <input
+                    placeholder={l.text}
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                  ></input>
+                  <button
+                    className="bg-blue-500 rounded-md w-1/8 m-0.5"
+                    onClick={() => saveEditList()}
+                  >
+                    save
+                  </button>
+                </>
+              ) : (
+                <>
+                  {" "}
+                  <p className="w-7/8">{l.text}</p>
+                  <button
+                    className="bg-blue-500 rounded-md w-1/8 m-0.5"
+                    onClick={() => deleteList(l.id)}
+                  >
+                    delete
+                  </button>
+                  <button
+                    className="bg-gray-200 rounded-md w-1/8 m-0.5"
+                    onClick={() => {
+                      setEditId(l.id);
+                    }}
+                  >
+                    edit
+                  </button>
+                </>
+              )}
+            </li>
+          ))}
         </ul>
       </div>
     </>
@@ -33,3 +108,10 @@ function App() {
 }
 
 export default App;
+
+//akış
+/*
+1. edit moduna geç(listeden inputa dön,kaydet butonu ekle)
+2. yazıyı düzenle
+3. kaydet
+*/
